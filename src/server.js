31ffe9ea08377b35601f0d5d20f7cdd11d220a20
Felix <felixsavero78@gmail.com>
2023-01-07 12:47:17 +0700

    Implemented postgres database

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
+require('dotenv').config(); // this will read .env and enable us to read the content from process.env
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+// Method accepts an object, destructures it, then maps it to a new object which will be returned
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  created_at,
+  updated_at,
+}) => ({
+  id,
+  title,
+  body,
+  tags,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+module.exports = { mapDBToModel };
